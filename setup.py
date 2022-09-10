3013060f28656c168ae58f8685fcf6940f6addc6
RFA <uergashev@rfa.com>
2022-09-10 12:54:02 -0400

    setup.py updated

--- setup.py
+++ setup.py
@@ -1,34 +1,32 @@
 #!/usr/bin/env python
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="tap-airflow",
-    version="0.1.0",
-    description="Singer.io tap for extracting data",
-    author="Stitch",
-    url="http://singer.io",
+    version="1.0.0",
+    description="Singer.io tap for extracting data from the airflow API",
+    author="Applied Labs",
     classifiers=["Programming Language :: Python :: 3 :: Only"],
-    py_modules=["airflow"],
+    py_modules=["tap_airflow"],
     install_requires=[
-        # NB: Pin these to a more specific version for tap reliability
-        "apache-airflow-client == 2.3.0",
-        "backoff == 1.8.0",
-        "ciso8601 == 2.2.0",
-        "jsonschema == 2.6.0",
-        "python-dateutil == 2.8.2",
-        "pytz == 2022.2.1",
-        "simplejson == 3.11.1",
-        "singer-python == 5.12.2",
-        "six == 1.16.0",
-        "urllib3 == 1.26.12"
+        "apache-airflow-client==2.3.0",
+        "backoff==1.8.0",
+        "ciso8601==2.2.0",
+        "jsonschema==2.6.0",
+        "python-dateutil==2.8.2",
+        "pytz==2022.2.1",
+        "simplejson==3.11.1",
+        "singer-python==5.12.2",
+        "six==1.16.0",
+        "urllib3==1.26.12"
     ],
-    entry_points="""
+    entry_points='''
     [console_scripts]
-    tap-airflow=airflow:main
-    """,
-    packages=["airflow"],
+    tap-airflow=tap_airflow:main
+    ''',
+    packages=find_packages(),
     package_data={
-        "schemas": ["airflow/schemas/*.json"]
-    },
-    include_package_data=True,
-)
+        'tap_airflow': [
+            'schemas/*.json'
+        ]
+    })
